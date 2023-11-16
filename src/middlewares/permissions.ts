2cd5a1dce51ab27efad4ef4100a251f9439c0196
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2023-11-16 22:07:16 +0545

    better authorization

--- src/middlewares/permissions.ts
+++ src/middlewares/permissions.ts
@@ -3,15 +3,23 @@ import { GraphQLError } from "graphql";
 import { shield, rule } from "graphql-shield";
 import { YogaInitialContext } from "graphql-yoga";
 
-const isAuthenticated = rule()(async (_, __, context: YogaInitialContext) => {
+const isAuthenticated = rule()(async (_, args, context: YogaInitialContext) => {
 	const sessionId = context.request.headers.get("authorization")?.split(" ")[1];
-	if (!sessionId) return false;
+	if (!sessionId) return new GraphQLError("No session id provided");
 
 	const session = await clerkClient.sessions.getSession(sessionId);
-	if (!session) return false;
+	if (!session) return new GraphQLError("Session not found");
+
+	if (args.userId) {
+		const userFromClerkSession = await clerkClient.users.getUser(session.userId);
+
+		if (userFromClerkSession.id !== args.userId) {
+			return new GraphQLError("User id does not match with the session user id");
+		}
+	}
 
 	const expiryDate = new Date(session.expireAt);
-	if (expiryDate < new Date()) return false;
+	if (expiryDate < new Date()) return new GraphQLError("Session expired");
 
 	return true;
 });
@@ -31,7 +39,7 @@ const permissions = shield(
 				return new GraphQLError(error.message);
 			}
 
-			return new GraphQLError("Internal server error");
+			return new GraphQLError("Authorization Error");
 		},
 	},
 );
