047198390f8b767b9821146d2fa7c829c422d8d0
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2023-12-18 14:50:14 +0545

    check date functions added

---
+++ src/lib/check-date.ts
@@ -0,0 +1,19 @@
+export function checkIfToday(date: Date | null | undefined) {
+	if (!date) return false;
+	const today = new Date();
+	return (
+		date.getDate() === today.getDate() &&
+		date.getMonth() === today.getMonth() &&
+		date.getFullYear() === today.getFullYear()
+	);
+}
+
+export function checkIfOlderThanToday(date: Date | null | undefined) {
+	if (!date) return false;
+	const today = new Date();
+	return (
+		date.getDate() < today.getDate() &&
+		date.getMonth() <= today.getMonth() &&
+		date.getFullYear() <= today.getFullYear()
+	);
+}
