0ecae8addbba6f1bc6f78862a7ad8bab953fcb60
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2023-12-19 17:54:06 +0545

    user streak works

--- src/lib/streak.ts
+++ src/lib/streak.ts
@@ -7,12 +7,20 @@ import { checkIfToday } from "./check-date";
 export const increaseGoalStreak = async (goalId: number) => {
 	try {
 		const goals = await db
-			.select()
+			.select({
+				streak: goal.streak,
+				streakUpdatedAt: goal.streakUpdatedAt,
+				userId: goal.userId,
+				goalId: goal.goalId,
+				isActive: goal.isActive,
+			})
 			.from(goal)
 			.where(and(eq(goal.goalId, goalId), eq(goal.isActive, true)));
 
 		const singleGoal = goals[0];
-		if (!checkIfToday(singleGoal.streakUpdatedAt)) {
+		console.log(singleGoal);
+
+		if (!checkIfToday(singleGoal.streakUpdatedAt) && singleGoal.isActive) {
 			const date = new Date();
 
 			await db
@@ -24,7 +32,7 @@ export const increaseGoalStreak = async (goalId: number) => {
 				.where(eq(goal.goalId, goalId));
 		}
 
-		await increaseUserStreak(singleGoal.userId);
+		await increaseUserStreak(singleGoal.userId, singleGoal.goalId);
 	} catch (error) {
 		if (error instanceof Error) {
 			console.log("Error in increasing goal streak of goalId " + goalId + ": ", error.message);
@@ -32,14 +40,23 @@ export const increaseGoalStreak = async (goalId: number) => {
 	}
 };
 
-async function increaseUserStreak(userId: string) {
-	const userStreaks = await db
-		.select({ streak: user.streak, streakUpdatedAt: user.streakUpdatedAt })
+async function increaseUserStreak(userId: string, goalId: number) {
+	const userWithGoalArr = await db
+		.select()
 		.from(user)
+		.innerJoin(goal, and(eq(goal.userId, user.userId), eq(goal.goalId, goalId)))
 		.where(eq(user.userId, userId));
+	const singleUser = userWithGoalArr[0];
 
-	const userStreak = userStreaks[0];
-	const streakUpdatedAt = new Date(
-		userStreak.streakUpdatedAt?.toLocaleDateString() || "",
-	).getTime();
+	if (!checkIfToday(singleUser.account.streakUpdatedAt)) {
+		const date = new Date();
+
+		await db
+			.update(user)
+			.set({
+				streak: singleUser.account.streak + 1,
+				streakUpdatedAt: date,
+			})
+			.where(eq(user.userId, userId));
+	}
 }
