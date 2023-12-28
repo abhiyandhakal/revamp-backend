84e088d9ab3392016faa85b75038a7db0a3eefc8
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2023-12-29 01:35:06 +0545

    remove unwanted userID from worked-on-log

--- src/db/schema/worked-on-log.ts
+++ src/db/schema/worked-on-log.ts
@@ -1,13 +1,9 @@
 import { pgTable, integer, timestamp } from "drizzle-orm/pg-core";
-import { user } from "./user";
 import { goal } from "./goal";
 import { task } from "./task";
 
 export const workedOnLog = pgTable("worked-on-log", {
 	date: timestamp("date").defaultNow(),
-	userId: integer("userId")
-		.notNull()
-		.references(() => user.userId),
 	taskId: integer("taskId")
 		.notNull()
 		.references(() => task.taskId),
